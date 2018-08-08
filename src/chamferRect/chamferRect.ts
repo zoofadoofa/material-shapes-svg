b30018684f3f0f28eb19dc655011dfccd3bd8948
Derek Moss <derek.randall.moss@gmail.com>
2018-08-08 16:19:18 -0500

    cleaned up project in preperation for building publish pipeline

--- src/chamferRect/chamferRect.ts
+++ src/chamferRect/chamferRect.ts
@@ -1,58 +1,59 @@
 import * as SVG from 'svg.js';
 
+/**
+ * Given the hypotenuse of a isosceles triangle that is right angled, find the length of a side.
+ */
+const isoscelesRightTriangleLengthOfSide = function(hypotenuse: number): number {
+    // hypotenuse / sqrt(2)
+    return hypotenuse * 0.7072;
+}
+
+
+type RectCorner = 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left';
+
+const cutCorner = function(
+    x: number,
+    y: number,
+    cut: number,
+    corner: RectCorner,
+    isStart: boolean
+): string {
+    const sideLength = isoscelesRightTriangleLengthOfSide(cut);
+    const start = isStart ? 'M' : 'L';
+
+    switch(corner) {
+        case 'top-left': {
+            return `
+            ${start} ${x} ${y + sideLength}
+            L ${x + sideLength} ${y}
+            `
+        }
+        case 'top-right': {
+            return `
+            ${start} ${x - sideLength} ${y}
+            L ${x} ${y + sideLength}
+            `
+        }
+        case 'bottom-right': {
+            return `
+            ${start} ${x} ${y - sideLength}
+            L ${x - sideLength} ${y}
+            `
+        }
+        case 'bottom-left': {
+            return `
+            ${start} ${x + sideLength} ${y}
+            L ${x} ${y - sideLength}
+            `
+        }
+    }
+}
+
 const MDSChamferRect = SVG.invent({
     create: 'path',
     inherit: SVG.Path,
     extend: {
         chamfer: function(width: number, height: number, cut: number): SVG.MDSChamferRect {
-            type RectCorner = 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left';
-
-            /**
-             * Given the hypotenuse of a isosceles triangle that is right angled, find the length of a side.
-             */
-            const isoscelesRightTriangleLengthOfSide = function(hypotenuse: number): number {
-                // hypotenuse / sqrt(2)
-                return hypotenuse * 0.7072;
-            }
-
-            const cutCorner = function(
-                x: number,
-                y: number,
-                cut: number,
-                corner: RectCorner,
-                isStart: boolean
-            ): string {
-                const sideLength = isoscelesRightTriangleLengthOfSide(cut);
-                const start = isStart ? 'M' : 'L';
-
-                switch(corner) {
-                    case 'top-left': {
-                        return `
-                        ${start} ${x} ${y + sideLength}
-                        L ${x + sideLength} ${y}
-                        `
-                    }
-                    case 'top-right': {
-                        return `
-                        ${start} ${x - sideLength} ${y}
-                        L ${x} ${y + sideLength}
-                        `
-                    }
-                    case 'bottom-right': {
-                        return `
-                        ${start} ${x} ${y - sideLength}
-                        L ${x - sideLength} ${y}
-                        `
-                    }
-                    case 'bottom-left': {
-                        return `
-                        ${start} ${x + sideLength} ${y}
-                        L ${x} ${y - sideLength}
-                        `
-                    }
-                }
-            }
-
             const d = `
                 ${cutCorner(0, 0, cut, 'top-left', true)}
                 ${cutCorner(width, 0, cut, 'top-right', false)}
