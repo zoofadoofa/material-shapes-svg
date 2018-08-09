6b669ddbb6da5be85e4d38cd191caf9e99345c24
Derek Moss <derek.randall.moss@gmail.com>
2018-08-09 12:52:16 -0500

    fixed demo code

--- app.ts
+++ app.ts
@@ -1,11 +1,6 @@
 import * as svgjs from 'svg.js';
 import 'svg.filter.js';
 
-import {
-    InitialPath, GetHeight
-} from './src/app-bar/app-bar-bottom';
-
-
 import './src/chamferRect/chamferRect';
 import './src/ripple/ripple';
 import './src/elevation/elevation';
@@ -13,18 +8,6 @@ import './src/elevation/elevation';
 import { raisedButton } from './demo/example-button/example-button';
 import { raisedDiamond } from './demo/example-diamond/example-diamond';
 import { exampleRipple } from './demo/example-ripple/example-ripple';
-
-
-const toolbar1: HTMLElement = document.querySelector('div#toolbar1');
-const toolbar2: HTMLElement = document.querySelector('div#toolbar2');
-const width1 = toolbar1.clientWidth;
-const width2 = toolbar2.clientWidth;
-const draw1 = svgjs('toolbar1-background').size(width1, GetHeight());
-const draw2 = svgjs('toolbar2-background').size(width2, GetHeight());
-draw1.path(InitialPath('expanded', 'center', width1))
-.addClass('tmdc-app-bar-bottom-path');
-draw2.path(InitialPath('expanded', 'end', width2))
-.addClass('tmdc-app-bar-bottom-path');
 
 setTimeout(() => {
 
